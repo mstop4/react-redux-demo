d8036347f3822eaac0533cc8e70e7aaef097c9b7
mstop4 <mstop4@gmail.com>
2018-01-19 15:55:15 -0500

    modified FormContainer.js

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -1,18 +1,33 @@
 import react, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Input from '../presentation/Input'
 
 class FormContainer extends Component {
   constructor() {
     super()
 
     this.state = {
-      title: ''
+      seo_title: ''
     }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange() {
+    this.setState({ [event.target.id]: event.target.value })
   }
 
   render() {
+    const { seo_title } = this.state
     return {
       <form id="article-form">
+        <Input
+          text="SEO Title"
+          label="seo_Title"
+          type="text"
+          id="seo_Title"
+          value={seo_title}
+          handleChange={this.handleChange}
       </form>
     }
   }
